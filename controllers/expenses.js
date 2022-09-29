0ef50a9844629891d63277800f359da25c57577d
aaron-calahorra <cal21082@byui.edu>
2022-09-28 22:41:54 -0600

    PUT and DELETE

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -50,8 +50,51 @@ const createExpense = async (req, res) => {
     }
 };
 
+const updateExpense = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .replaceOne({ _id: userId }, contact);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteExpense = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
-    getAll,
-    getSingle,
-    createExpense
+  getAll,
+  getSingle,
+  createExpense,
+  updateExpense,
+  deleteExpense
 };
